f73da3b8029cd668dc2cdc2b5a26ddb34ebb1780
Opeyemi Sanusi <opeyemisanusi@gmail.com>
2022-02-16 23:49:12 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,23 @@
 ## functions do
 
 ## Write a short comment describing this function
+## 'makeCacheMatrix' takes in a matrix called 'x'
+## it also takes 'var_inv' which we would use to store the inverse of our matrix
+## 'var_inv' needs to be set to null
+## I then created another function 'set' that takes in the values of x and assigns it
+## get - gets the value of x by calling it
+## setInverse - this would take in the an inverse and set it to 'var_inv'
+## getInverse - would pull out the value of 'var_inv'
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  var_inv <- NULL
+  set <- function(y){
+    x <<- y
+    var_inv <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) var_inv <<- inverse
+  getInverse <- function() var_inv
 }
 
 

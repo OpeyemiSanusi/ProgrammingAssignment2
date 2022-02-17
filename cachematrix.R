4dcf72ab9a369b3c7a276a73fc11ce22f1448b27
Opeyemi Sanusi <opeyemisanusi@gmail.com>
2022-02-17 12:34:30 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+##The function takes in a matrix, converts it to an inverse
+## It then stores the value of the inverse so it does not have to recompute
+##...when that same matrix is called
 
-## Write a short comment describing this function
+
+
+
 ## 'makeCacheMatrix' takes in a matrix called 'x'
 ## it also takes 'var_inv' which we would use to store the inverse of our matrix
 ## 'var_inv' needs to be set to null
@@ -9,8 +12,13 @@
 ## get - gets the value of x by calling it
 ## setInverse - this would take in the an inverse and set it to 'var_inv'
 ## getInverse - would pull out the value of 'var_inv'
+## put the results in a list so it returns the result of all the function
+## so we can call it
+## ... allows you to pass a function that is not defined in the function arg
+## if use call the function and pass in for example 'na.rm' this would replace
+## all the ... in the function to na.rm
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix(), ...) {
   var_inv <- NULL
   set <- function(y){
     x <<- y
@@ -19,26 +27,34 @@ makeCacheMatrix <- function(x = matrix()) {
   get <- function()x
   setInverse <- function(inverse) var_inv <<- inverse
   getInverse <- function() var_inv
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
+
 }
 
 
-## Write a short comment describing this function
-## ... allows you to pass a function that is not defined in the function arg
-## if use call the function and pass in for example 'na.rm' this would replace
-## all the ... in the function to na.rm
+
+
+## cacheSolve takes in a matrix as x 
+## it assigns x$getInverse() (from makeCacheMatrix) to var_inv
+## if x$getInverse does not have a value yet it would return NULL
+## the function then checks if var_inv is not empty
+## if that's true it fetches the existing data contained in var_inv
+## if not in fetches the matrix provided then inverts it and assigns it to var_inv
+## it then sets the var_inv to the new value and prints the inverse
 
 cacheSolve <- function(x, ...) { 
         ## Return a matrix that is the inverse of 'x'
-  j <- x$getInverse
-  if(!is.null(j)){
+  var_inv <- x$getInverse()
+  if(!is.null(var_inv)){
     message("getting cached data")
-    return(j)
+    return(var_inv)
   }
-  else {
-    mat <- x$get
-    j <- solve(mat, ...)
-    x$setInverse(j)
-    j
-  }
+    message("creating it now")
+    mat <- x$get()
+    var_inv <- solve(mat, ...)
+    x$setInverse(var_inv)
+    var_inv
 }
 
